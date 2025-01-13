95af94ec86a13d07dbdf53741985bade5c45a7de
Iyed13tns <m@g.com>
2025-01-13 09:44:16 +0100

    holberton

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,9 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Décrémenter n pour éviter une boucle infinie
+        n -= 1  # Décrémenter n pour éviter la boucle infinie
     return result
 
 f = factorial(int(sys.argv[1]))
 print(f)
-
